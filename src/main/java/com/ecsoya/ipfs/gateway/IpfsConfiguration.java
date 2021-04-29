833fb24d9e8a5387ecabc42180ec4a93968bb417
AngryRED <angryred@qq.com>
2021-04-29 14:35:02 +0800

    Add auto config for spring boots

---
+++ src/main/java/com/ecsoya/ipfs/gateway/IpfsConfiguration.java
@@ -0,0 +1,8 @@
+package com.ecsoya.ipfs.gateway;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = "com.ecsoya.ipfs.gateway")
+public class IpfsConfiguration {
+
+}
